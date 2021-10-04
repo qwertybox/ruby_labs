ab61c65638c84b3dd8d4f81aaafd9eb2003adc26
qwertybox <49982199+qwertybox@users.noreply.github.com>
2021-10-04 20:13:12 +0300

    classes 2

--- test2.rb
+++ test2.rb
@@ -25,3 +25,27 @@ bird.move("tree")
 dog.talk
 bird.talk
 cat.move("house")
+#local methods: if we set variable inside method it keep till method is not ended it's execution
+# object variable @variable, local(method) variable - variable without @
+# access methods
+class Tower
+  def name= (value)
+    @name = value
+  end
+  def name
+    @name
+  end
+  def height=(value)
+    @Height = value
+  end
+  def height
+    @Height
+  end
+  def report_info
+    puts "The name of Tower is #{name} and Height is #{height}"
+  end
+end
+Tower1 = Tower.new
+Tower1.name = "Piza"
+Tower1.height = 566
+Tower1.report_info
