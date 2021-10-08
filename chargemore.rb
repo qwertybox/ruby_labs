cbfd10f1a1b78fa6bc740097e427aeae9170b7c5
qwertybox <49982199+qwertybox@users.noreply.github.com>
2021-10-08 19:03:01 +0300

    initialize

--- chargemore.rb
+++ chargemore.rb
@@ -27,9 +27,52 @@ class Employee
     "#{@name}, salary is #{@salary}"
   end
 end
+class SalariedEmployee < Employee
+  attr_reader :salary
+  def initialize(name = "Anonymous", salary = 0.0) self.name = name
+   self.salary = salary
+  end
+  def salary=(salary)
+    super
+  end
+  def print_pay_stub
+    pay_for_period = (salary / 365.0) * 14
+    formatted_pay = format("$%.2f", pay_for_period)
+    puts "Pay This Period: #{formatted_pay}"
+  end
+end
+class HourlyEmployee < Employee
+  attr_reader :hourly_wage, :hours_per_week
+  def initialize(name = "Anonymous", hourly_wage = 0.0, hours_per_week = 0.0) self.name = name
+   self.hourly_wage = hourly_wage
+   self.hours_per_week = hours_per_week
+  end
+  def hourly_wage=(hourly_wage)
+    # Код проверки и присваивания @hourly_wage
+  end
+  def hours_per_week=(hours_per_week)
+    # Код проверки и присваивания @hours_per_week
+  end
+  def print_pay_stub
+    pay_for_period = hourly_wage * hours_per_week * 2
+    formatted_pay = format("$%.2f", pay_for_period)
+    puts "Pay This Period: #{formatted_pay}"
+  end
+end
 amy = Employee.new
 puts amy
 amy.print_pay_stub #here are no cents
+salaried_employee = SalariedEmployee.new
+salaried_employee.name = "Jane Doe"
+salaried_employee.salary = 50000
+puts salaried_employee
+salaried_employee.print_pay_stub
+
+hourly_employee = HourlyEmployee.new
+hourly_employee.name = "John Smith"
+hourly_employee.hourly_wage = 14.97
+hourly_employee.hours_per_week = 30
+hourly_employee.print_pay_stub
 #####
 # nil in ruby its the same as null in C#
 # все математические операторы передаются в виде методов
