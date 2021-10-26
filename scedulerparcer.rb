be505e9a35592e645d187e38c8493a2bc64d2c13
qwertybox <49982199+qwertybox@users.noreply.github.com>
2021-10-26 23:56:04 +0300

    sceduleparser modified

--- scedulerparcer.rb
+++ scedulerparcer.rb
@@ -9,7 +9,7 @@ class ScedulerParser
     'Sun' => 'Sunday'
   }.freeze
 
-  def initialize (path)
+  def initialize(path)
     readfile path
   end
 
@@ -23,7 +23,7 @@ class ScedulerParser
 
   private
 
-  def readfile (path)
+  def readfile(path)
     file = File.open(path)
     @lines = file.read
     file.close
@@ -34,8 +34,8 @@ class ShopParser < ScedulerParser
 
   def parse_file
     @rezult = {}
-    @lines.each_line do |row|
-      name, time_line = name_scedule row
+    @lines.each_line do |line|
+      name, time_line = name_scedule(line)
       @rezult[name] = time_line#@rezult << {'name' => name, 'time_line' => time_line
     end
     puts @rezult
@@ -50,7 +50,7 @@ class ShopParser < ScedulerParser
 
   private
 
-  def prnt_daytime (timeline)
+  def prnt_daytime(timeline)
     timeline.each do |day_time|
       day_time.each_pair do |day, time|
         day = WEEKDAYS[day]
@@ -63,15 +63,15 @@ class ShopParser < ScedulerParser
     end
   end
 
-  def name_scedule (row)
-    name = row.chomp.split(': ')[0] #  name
-    timeline = parse_timeline(row.chomp.split(': ')[1].split(';'))
+  def name_scedule(line)
+    name = line.chomp.split(': ')[0] #  name
+    timeline = parse_timeline(line.chomp.split(': ')[1].split(';'))
     #[name], [timeline]
     # timeline = {Monday,[t1,t2]}, {Tuesday,[t1,t2]}.....
     return name, timeline
   end
 
-  def parse_timeline (days_times) # array day:time
+  def parse_timeline(days_times) # array day:time
     # return {Monday,[t1,t2]}, {Tuesday,[t1,t2]}....
     timelin_arr = []
     days_times.each do |day_time|
@@ -82,18 +82,18 @@ class ShopParser < ScedulerParser
     timelin_arr
   end
 
-  public def parse_day (day_time)
+  public def parse_day(day_time)
     day_time.split(':')[0]
   end
 
-   def parse_time (time)
+   def parse_time(time)
     return 'off', 'off' if time == 'off'
     time1 = putdots(time.split('-')[0])
     time2 = putdots(time.split('-')[1])
     return time1, time2
   end
 
-  def putdots (string)
+  def putdots(string)
     "#{string[0..1]}:#{string[2..3]}"
   end
 end
